b2228588d5d1d6e9186c30ca98c8d4aaebff25ed
Agustin Pietrafesa <ahpietrafesa@gmail.com>
2023-08-01 12:29:35 -0300

    Avanzando en la preentrega

--- source/Products/controller.js
+++ source/Products/controller.js
@@ -1,15 +1,38 @@
 /**************Controller de productos ********/
 const { Router } = require('express')
+const ProductManager = require('../classes/ProductManager')
 
 const router = Router()
+const managerProducts = new ProductManager()
 
-router.get('/', (req, res) => {
-    res.json({message: 'todos los productos'})
+router.get('/', async (req, res) => {
+    try {
+        const { limit } = req.query
+        const allProducts = await managerProducts.getProducts()
+        const productsFilter = allProducts.slice(0, limit || 10)
+        res.json({Productos: productsFilter})
+        
+    } catch (error) {
+        console.log(error)
+    }
 })
 
-router.get('/:id', (req, res) => {
-    res.json( { message: `el producto es ${req.params.id}`})
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const allProducts = await managerProducts.getProducts()
+        const productFind = allProducts.find(product => product.id === Number(id))
+        if(!productFind){
+            res.json(`No se encontro un producto con el id ${id}`)
+        }else{
+            return res.json({Producto: productFind})
+        }
+        
+    } catch (error) {
+        console.log(error)
+    }
 })
+
 
 router.post('/', (req, res) => {
     console.log(req.body)
---
+++ source/classes/ProductManager.js
@@ -0,0 +1,140 @@
+const fs = require('fs')
+
+class ProductManager {
+    path
+    products
+
+    constructor(){
+        this.products = []
+        this.path = './files/Products.JSON'
+
+    };
+    productId = 0 
+
+
+    /*********Metodo para agregar productos nuevos la lista *************/
+    async addProduct(title, description, price, thumbnail, code, stock) {
+        try {
+            this.productId++;
+            const product = {
+                title,
+                description,
+                price,
+                thumbnail,
+                code,
+                stock,
+                id: this.productId
+            }   
+            /*******Generamos la obligatoriedad de los parametros  *********/
+            const allParams = [title, description, price, thumbnail, code, stock]
+            const reqParams = allParams.every(value => value);
+    
+            if ( !reqParams ){
+                console.warn('Todos los parametros deben completarse')
+                return;
+            }
+            /********Generamos la singulairdad del code ******/
+            const checkCode = this.products.find(productCode => productCode.code === code)
+    
+            if(checkCode) {
+                console.log(`El producto con el codigo ${checkCode.code} ya esta ingresado en el sistema.`)
+            } else {            
+                this.products.push(product);
+            }
+    
+    
+            /**************Escribimos el archivo JSON con los datos de los productos *******/
+             await fs.promises.writeFile(this.path, JSON.stringify(this.products))
+            
+            
+        } catch (error) {
+            console.log(error)
+        }
+
+     };
+
+    /*********Metodo que retorna toda la lista de los productos que haya hasta el momento *******/
+    async getProducts() {
+        try {
+            if(fs.existsSync(this.path)){ 
+                const data = await fs.promises.readFile(this.path, 'utf-8')
+                this.products = JSON.parse(data)
+                return this.products ? this.products : []
+            }else{
+                return []
+            };            
+        } catch (error) {
+            console.log(error)
+        }    
+
+    }  
+    /*********metodo que busca producto mediante un ID pasado como parametro ******/
+    async getProductById(id) {
+        try {
+            if(fs.existsSync(this.path)){
+                const data = await fs.promises.readFile(this.path, 'utf-8')
+                const ourProducts = JSON.parse(data)
+                const productoBuscado = ourProducts.find(product => product.id === id)
+                productoBuscado ? console.log(productoBuscado) : console.log('Product Not Found');
+            }else{
+                console.log('Lo siento algo salio mal')
+            };         
+
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
+    /************Metodo que busca un producto por el id y modifica uno o todos sus campos ******/
+
+    async updateProduct(id, campo, valor){
+        try {
+            if(fs.existsSync(this.path)){
+                const data = await fs.promises.readFile(this.path, 'utf-8')
+                const ourProducts = JSON.parse(data)
+                const productoBuscado = ourProducts.find(product => product.id === id)
+                await this.deleteProduct(id)
+                let key = campo
+                productoBuscado[key] = valor
+                productoBuscado ? this.products.push(productoBuscado) : console.log('Product Not Found');
+                await fs.promises.writeFile(this.path, JSON.stringify(this.products))
+
+            }else{
+                console.log('Lo siento algo salio mal')
+            };         
+ 
+        } catch (error) {
+            console.log(error)
+        }
+
+
+    }
+
+    /**************Metodo que busca un producto por su id y lo elimina del array ***************/
+
+    async deleteProduct(id) {
+        const data = await fs.promises.readFile(this.path, 'utf-8')
+        const ourProducts = JSON.parse(data)
+        const productIndex = ourProducts.findIndex(product => product.id === id)
+
+        productIndex < 0 
+        ?
+        console.log('No se encuentra el producto que desea eliminar')
+        :
+        ourProducts.splice(productIndex, 1); 
+        this.products = await ourProducts
+        const datos1 = await fs.promises.writeFile(this.path, JSON.stringify(this.products))
+
+    }
+
+
+};
+ 
+/*********Exportamos la clase ********/
+
+module.exports = ProductManager
+
+
+
+
